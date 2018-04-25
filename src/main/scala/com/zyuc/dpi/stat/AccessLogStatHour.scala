14af8c329af20e310a6a6ce09268e0e949a6af94
zhoucw <cqfrog@163.com>
2018-04-25 10:51:26 +0800

    statHour

--- src/main/scala/com/zyuc/dpi/stat/AccessLogStatHour.scala
+++ src/main/scala/com/zyuc/dpi/stat/AccessLogStatHour.scala
@@ -21,20 +21,23 @@ object AccessLogStatHour {
     val outputParentPath = sc.getConf.get("spark.app.outputParentPath", "/hadoop/accesslog_stat/hour/out")
 
 
-/*    // spark sql 使用正则表达式有bug, 使用自定义的udf，正则表达式参考原pig程序
-    val udf_isDomain = udf({
-      val pattern = "^[\\w\\-:.]+$"
-      (s: String) => pattern.r.pattern.matcher(s.trim).matches() match {
-        case true =>
-          1
-        case _ => {
-          0
-        }
-      }
-    })*/
+    /*    // spark sql 使用正则表达式有bug, 使用自定义的udf，正则表达式参考原pig程序
+        val udf_isDomain = udf({
+          val pattern = "^[\\w\\-:.]+$"
+          (s: String) => pattern.r.pattern.matcher(s.trim).matches() match {
+            case true =>
+              1
+            case _ => {
+              0
+            }
+          }
+        })*/
+
+
+
 
     //#############################################
-    //   广播域名
+    //   广播域名基础数据
     //#############################################
     val domainInfo = sc.textFile("/hadoop/basic/domainInfo.txt").
       map(x => x.split("\\t")).filter(_.length == 3).map(x => (x(0), x(1)))
@@ -46,17 +49,19 @@ object AccessLogStatHour {
     val bd_areaDomain = sc.broadcast(areaDomain)
     val bd_countryDomain = sc.broadcast(countryDomain)
 
+
     //#############################################
     //  自定义UDF函数
     //  功能： 1. 判断域名是否合法
     //        2. 根据域名获取顶级域名
+    //  return:  -1:不规范域名  空: 无顶级域名（如ip地址）
     //#############################################
     val bdv_inDomain = bd_inDomain.value
     val bdv_areaDomain = bd_areaDomain.value
     val bdv_countryDomain = bd_countryDomain.value
     val regExpr = "^[\\w\\-:.]+$"
     val pattern = regExpr.r.pattern
-    spark.udf.register("udf_isDomain", (domain: String) => {
+    spark.udf.register("udf_topDomain", (domain: String) => {
       var topDomain = ""
       // 首先判断域名是否合法
       pattern.matcher(domain.trim).matches() match {
@@ -64,8 +69,8 @@ object AccessLogStatHour {
           val arrDomain = domain.split("\\.")
           val len = arrDomain.length
           var last_1 = "-1" // 最后一位
-        var last_2 = "-1" // 倒数第二位
-        var last_3 = "-1" // 倒数第三位
+          var last_2 = "-1" // 倒数第二位
+          var last_3 = "-1" // 倒数第三位
 
           if (len > 2) {
             last_1 = arrDomain(len - 1)
@@ -92,7 +97,6 @@ object AccessLogStatHour {
         }
       }
     })
-
 
 
     //#############################################
@@ -123,30 +127,31 @@ object AccessLogStatHour {
          |select hip from ${houseIpTmpView}
        """.stripMargin)
 
-   //#############################################
-   //   关联是否合法ip
-   //#############################################
+    //#############################################
+    //   关联顶级域名/是否合法ip
+    //#############################################
     val accessAndIpSql =
-      s"""
-         |select a.*, udf_isDomain(a.domain) topdomain,
-         |       case when h.hip is null then 0 else 1 end iflegal
-         |from ${accessTable} a left join ${houseIpTable} h
-         |on( a.destip = h.hip )
+    s"""
+       |select a.*, udf_topDomain(a.domain) topdomain,
+       |       case when h.hip is null then 0 else 1 end iflegal
+       |from ${accessTable} a left join ${houseIpTable} h
+       |on( a.destip = h.hip )
        """.stripMargin
 
     val accessAndIpTable = "accessAndIp"
     spark.sql(accessAndIpSql).createOrReplaceTempView(accessAndIpTable)
 
+
     //#############################################
-    //   1. 违法访问日志
+    //   1. 违法访问日志： 过滤不规范的域名
     //#############################################
     val illegalLogSql =
-      s"""
-         |select distinct '$hid' hid, domain, destip
-         |from ${accessAndIpTable}
-         |where iflegal = 0 and topdomain!='-1'
+    s"""
+       |select distinct '$hid' hid, domain, destip
+       |from ${accessAndIpTable}
+       |where iflegal = 0 and topdomain!='-1'
        """.stripMargin
-    val illegalLogPath = outputParentPath + "/illegLog/hid=" + hid + "/d=" + d + "/h=" +h
+    val illegalLogPath = outputParentPath + "/illegLog/hid=" + hid + "/d=" + d + "/h=" + h
     spark.sql(illegalLogSql).repartition(10).write.mode(SaveMode.Overwrite).format("orc").save(illegalLogPath)
 
 
@@ -154,17 +159,17 @@ object AccessLogStatHour {
     //   2. ip+port 统计
     //#############################################
     val accessIpPortSql =
-      s"""
-         |select '$hid' as hid, destip, destport, proctype,
-         |       min(acctime) as firsttime,
-         |       max(acctime) as activetime,
-         |       count(*) as times
-         |from ${accessAndIpTable}
-         |where iflegal=1
-         |group by destip, destport, proctype
-         |order by times desc
+    s"""
+       |select '$hid' as hid, destip, destport, proctype,
+       |       min(acctime) as firsttime,
+       |       max(acctime) as activetime,
+       |       count(*) as times
+       |from ${accessAndIpTable}
+       |where iflegal=1
+       |group by destip, destport, proctype
+       |order by times desc
        """.stripMargin
-    val ipPortPath = outputParentPath + "/ipport/hid=" + hid + "/d=" + d + "/h=" +h
+    val ipPortPath = outputParentPath + "/ipport/hid=" + hid + "/d=" + d + "/h=" + h
     spark.sql(accessIpPortSql).repartition(10).write.mode(SaveMode.Overwrite).format("orc").save(ipPortPath)
 
     spark.sql(s"select * from $accessAndIpTable ").show()
@@ -172,9 +177,41 @@ object AccessLogStatHour {
     //#############################################
     //   3. ip+domain 统计
     //#############################################
-    val accessIpDomainSql =
-      s"""
-         |
+    val accessDomainIpSql =
+    s"""
+       |select '$hid' as hid, topdomain, domain, destip,
+       |       min(acctime) as firsttime,
+       |       max(acctime) as activetime,
+       |       count(*) as times,
+       |       max(proctype) as proctype
+       |from ${accessAndIpTable}
+       |where iflegal=1 and topdomain!='-1'
+       |group by topdomain, domain, destip
        """.stripMargin
+    val domainIpPath = outputParentPath + "/domainip/hid=" + hid + "/d=" + d + "/h=" + h
+    spark.sql(accessDomainIpSql).repartition(10).write.mode(SaveMode.Overwrite).format("orc").save(domainIpPath)
+
+
+    //#############################################
+    //   4. domain 统计
+    //#############################################
+    val accessDomainDf = spark.read.format("orc").load(domainIpPath)
+    val accessDomainTable = "accessDomainTable"
+    accessDomainDf.createOrReplaceTempView(accessDomainTable)
+    val accessDomainSql =
+      s"""
+         |select '$hid' as hid, domain,
+         |       min(firsttime) as firsttime,
+         |       max(activetime) as activetime,
+         |       sum(times) as times,
+         |       max(topdomain) as topdomain,
+         |       max(destip) as destip
+         |from ${accessDomainTable}
+         |group by domain
+       """.stripMargin
+    val domainPath = outputParentPath + "/domain/hid=" + hid + "/d=" + d + "/h=" + h
+    spark.sql(accessDomainSql).repartition(10).write.mode(SaveMode.Overwrite).format("orc").save(domainPath)
+
+
   }
 }
