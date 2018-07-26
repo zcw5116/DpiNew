c715e7ef679bdd4892e499169c115e206a8a8cf6
zhoucw <cqfrog@163.com>
2018-07-26 17:33:29 +0800

    ETL support conplict dir

--- src/main/scala/com/zyuc/dpi/etl/AccesslogETL.scala
+++ src/main/scala/com/zyuc/dpi/etl/AccesslogETL.scala
@@ -4,7 +4,9 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import com.alibaba.fastjson.JSONObject
 import com.zyuc.dpi.etl.utils.AccessUtil
+import com.zyuc.dpi.etl.utils.AccessUtil._
 import com.zyuc.dpi.utils.{CommonUtils, FileUtils, JsonValueNotNullException}
+import com.zyuc.dpi.utils.FileUtils.renameHDFSDir
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.Logger
 import org.apache.spark.sql
@@ -49,6 +51,7 @@ object AccesslogETL {
   @throws(classOf[Exception])
   def doJob(parentContext: SQLContext, fileSystem: FileSystem, hiveDb: String, params: JSONObject): String = {
 
+    var result = ""
     try {
       var info = ""
       val appName = CommonUtils.getJsonValueByKey(params, "appName")
@@ -59,11 +62,12 @@ object AccesslogETL {
       val ifRefreshPartiton = CommonUtils.getJsonValueByKey(params, "ifRefreshPartiton")
       val tryTimeStr = CommonUtils.getJsonValueByKey(params, "tryTime")
       val loadTime = CommonUtils.getJsonValueByKey(params, "loadTime")
+      val ifDealConflictDir = CommonUtils.getJsonValueByKey(params, "ifDealConflictDir")
 
       val tryTime = tryTimeStr.toInt
       val coalesceSize = coalesceSizeStr.toInt
 
-      var result = "app:" + appName + ", tryTime:" + tryTime
+      result = "app:" + appName + ", tryTime:" + tryTime
 
       var begin = new Date().getTime
 
@@ -71,6 +75,10 @@ object AccesslogETL {
       sqlContext.sql("use " + hiveDb)
 
       val batchID = appName.substring(appName.lastIndexOf("_") + 1)
+      if(batchID.length<1){
+        result = result + s", the length of batchID must gt 0"
+        return result
+      }
 
       val partitions = "hid,d,h,m5"
 
@@ -86,42 +94,152 @@ object AccesslogETL {
       val inputDoingLocation = inputPath + "/" + batchID + "_doing"
       val inputDoneLocation = inputPath + "/" + batchID + "_done"
 
-      val srcExists = fileSystem.exists(new Path(inputLocation))
-      val srcDoingExists = fileSystem.exists(new Path(inputDoingLocation))
-      val srcDoneExists = fileSystem.exists(new Path(inputDoneLocation))
+     // val inputPathExists = fileSystem.exists(new Path(inputLocation))
+     // val inputPathDoingExists = fileSystem.exists(new Path(inputDoingLocation))
+     // val inputPathDoneExists = fileSystem.exists(new Path(inputDoneLocation))
 
-      if(srcDoneExists) {
-        fileSystem.globStatus(new Path(inputLocation + "/*")).foreach(x => {
-          val hidDir = x.getPath.getName
-          fileSystem.globStatus(new Path(s"${inputLocation}/$hidDir}/*")).foreach(f => {
-            val fname = f.getPath.getName
-            val newPath = new Path(inputDoneLocation + "/" + hidDir + "/" + fname)
-            fileSystem.rename(f.getPath, newPath)
-          })
-        })
 
+      /**
+        * 将冲突目录合并成一个目录， 目录的后缀为_doing, 根据返回结果判断是否需要处理：
+        *
+        * @return false-处理冲突目录失败  true-处理冲突目录成功
+        */
+      /*
+      def dealConflictDir(): Tuple2[Boolean, String] = {
+
+        var msg = ""
+        var conflictDir = ""
+        var ifSuccess: Boolean = true
+
+        var renameResult: Tuple2[Boolean, String] = (false, msg)
+
+        // 存在两个冲突的目录： do目录(未重命名的1分钟目录, 下同)和doing目录
+        if (inputPathExists && inputPathDoingExists && !inputPathDoneExists) {
+
+          conflictDir = s"${batchID}|${batchID}_doing"
+
+        }
+        // 存在两个冲突的目录： do目录和done目录
+        else if (inputPathExists && inputPathDoneExists && !inputPathDoingExists) {
+
+          conflictDir = s"${batchID}|${batchID}_done"
+          if (!renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)) {
+            ifSuccess = false
+            msg = s"rename from ${inputDoneLocation} to ${inputDoingLocation} failed"
+          }
+
+        }
+        // 存在两个冲突的目录： doing目录和done目录
+        else if (inputPathDoingExists && inputPathDoneExists && !inputPathExists) {
+
+          conflictDir = s"${batchID}_doing|${batchID}_done"
+          if (!renameHDFSDir(fileSystem, inputDoingLocation, inputLocation) || !renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)) {
+            ifSuccess = false
+            msg = s"rename from ${inputDoingLocation} to ${inputLocation} failed or rename from ${inputDoingLocation} to ${inputLocation} failed "
+          }
+
+        }
+        // 存在三个冲突的目录： do目录/doing目录和done目录
+        else if (inputPathDoingExists && inputPathDoneExists && inputPathExists) {
+
+          // 1. 先将doing目录下的文件移动到do目录下, 删除doing目录
+          // 2. 将done目录重命名为doing目录
+
+          conflictDir = s"${batchID}|${batchID}_doing|${batchID}_done"
+
+          if (!mvFiles(fileSystem, inputDoingLocation, inputLocation)) {
+            ifSuccess = false
+            msg = s"move files from ${inputDoingLocation} to ${inputLocation} failed"
+          }
+
+          if (!renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)) {
+            ifSuccess = false
+            msg = s"rename from ${inputDoneLocation} to ${inputDoingLocation} failed"
+          }
+
+        }
+        // 没有冲突的目录
+        else {
+          conflictDir = "no conflictDir"
+          ifSuccess = false
+        }
+
+        msg = s"conflictDir: ${conflictDir}, " + msg
+
+        // 将do目录下的文件移动到doing目录下
+        if (ifSuccess) {
+          if (!mvFiles(fileSystem, inputLocation, inputDoingLocation)) {
+            msg = msg + " move files from ${inputLocation} to ${inputDoingLocation} failed"
+            ifSuccess = false
+          }
+        }
+
+        (ifSuccess, msg)
+      }
+      */
+
+      /**
+        * 1. 判断目录是否冲突， 如果冲突，根据ifDealConflictDir判断是否处理冲突
+        * 2. 如果目录不冲突, 判断是否存在待处理的目录
+        * 3. 如果最终存在待处理的doing目录， 返回true
+        * @return
+        */
+      /*
+      def processDir(): Tuple2[Boolean, String] = {
+
+        var ifDeal = false
+        var msg = ""
+        // 判断是否有冲突的目录
+        if ((inputPathExists && inputPathDoingExists) || (inputPathExists && inputPathDoneExists) || (inputPathDoingExists && inputPathDoneExists)) {
+          // 是否处理冲突目录
+          if (ifDealConflictDir != "0") {
+           val dealResult = dealConflictDir()
+            if(dealResult._1){
+              ifDeal = true
+            }
+            msg = dealResult._2
+          } else{
+            msg = " no deal with conflictDir"
+          }
+
+        } else { // 不存在冲突目录
+          if (inputPathExists) {
+            if (renameHDFSDir(fileSystem, inputLocation, inputDoingLocation)) {
+              ifDeal = true
+            } else {
+              msg = s"rename $inputLocation to $inputDoingLocation failed"
+            }
+          }
+          else if (inputPathDoingExists && tryTime >= 1) {
+            ifDeal = true
+          } else {
+            msg = s"$inputLocation not exists "
+          }
+        }
+        (ifDeal, msg)
       }
 
 
-      val dirExists = fileSystem.exists(new Path(inputLocation))
-      if (!dirExists && tryTime == 0) {
-        logger.info(s"[$appName] $inputLocation not exists")
-        result = result + s" $inputLocation not exists"
+      // 对目录重命名等, 需要判断目录是否冲突
+      val processResult = processDir()
+      val processMsg = processResult._2
+      val ifProcessSucc = processResult._1
+      logger.info(s"[$appName], ${processMsg}")
+
+      if(!ifProcessSucc){
+        result = result + s" ${processMsg}"
         return result
-      } else if (!dirExists && tryTime > 0) {
-        if (!fileSystem.exists(new Path(inputDoingLocation))) {
-          logger.info(s"[$appName] $inputDoingLocation not exists")
-          result = result + s" $inputDoingLocation not exists"
-          return result
-        }
-      } else {
-        val isDoingRenamed = FileUtils.renameHDFSDir(fileSystem, inputLocation, inputDoingLocation)
-        if (!isDoingRenamed) {
-          logger.info(s"[$appName] $inputLocation move to $inputDoingLocation failed")
-          result = result + s" $inputLocation move to $inputDoingLocation failed"
-          return result
-        }
-        logger.info(s"[$appName] $inputLocation move to $inputDoingLocation success")
+      }
+*/
+
+      // 处理数据目录
+      val dealDirResult = AccessUtil.dealDir(fileSystem, inputPath, batchID, tryTime, ifDealConflictDir)
+      logger.info("[" + appName + "] 处理数据目录： 是否继续执行-" + dealDirResult._1 + ", 处理消息-" + dealDirResult._2)
+
+      // 是否执行后续操作， 不执行返回
+      if(!dealDirResult._1){
+        result = result + ", " + dealDirResult._2
+        return result
       }
 
       val sdf = new SimpleDateFormat("yyyyMMddHHmm")
@@ -229,17 +347,20 @@ object AccesslogETL {
     } catch {
       case jsonE: JsonValueNotNullException => {
         logger.error("[" + params + "]-" + jsonE.getMessage)
+        result = "[" + params + "]-" + jsonE.getMessage
         throw new JsonValueNotNullException("[" + params + "]-" + jsonE.getMessage)
         null
       }
       case e: Exception => {
         logger.error("[" + params + "]-" + e.getMessage)
         e.printStackTrace()
+        result = "[" + params + "]-" + e.getMessage
         throw new Exception("[" + params + "]-" + e.getMessage)
         null
       }
     }
 
   }
+
 
 }
--- src/main/scala/com/zyuc/dpi/etl/utils/AccessUtil.scala
+++ src/main/scala/com/zyuc/dpi/etl/utils/AccessUtil.scala
@@ -6,6 +6,9 @@ import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructT
 import java.util.Base64
 
 import com.zyuc.dpi.utils.ConfigUtil
+import com.zyuc.dpi.utils.FileUtils.renameHDFSDir
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.Logger
 
 /**
   * Created on 上午3:16.
@@ -33,8 +36,9 @@ object AccessUtil {
 
   /**
     * from unix time to yyyyMMddHHmmss
+    *
     * @param time
-    * @return   tuple4(time,d,h,m5)
+    * @return tuple4(time,d,h,m5)
     */
   def getTime(time: String): Tuple4[String, String, String, String] = {
     try {
@@ -55,7 +59,7 @@ object AccessUtil {
   /**
     *
     * @param line
-    * @return      Row
+    * @return Row
     */
   def parse(line: String) = {
     try {
@@ -90,39 +94,170 @@ object AccessUtil {
     } catch {
       case e: Exception => {
         val houseid = -1
-        Row(houseid, line, "-1", "-1", "-1", "-1", "-1", "-1", "-1","-1", "-1", "-1", "-1")
+        Row(houseid, line, "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1")
       }
     }
   }
 
   /**
     * 根据配置文件， 配置每个机房的分区大小
+    *
     * @deprecated
     * @param hid
     * @return
     */
-  def getPartitionSize(hid:String):Int = {
+  def getPartitionSize(hid: String): Int = {
     val config = ConfigUtil.getConfig("/home/slview/bigdata/app/spark/server/config/partition.conf")
     var partitionSize = config.getString("partitionSize.0")
-    try{
+    try {
       partitionSize = config.getString("partitionSize." + hid)
-    }catch {
-      case e:Exception => {
+    } catch {
+      case e: Exception => {
         //e.printStackTrace()
       }
     }
     partitionSize.toInt
   }
 
+  /**
+    * 将文件移动到目标目录， 子目录为机房id。 如果源目录为空， 做返回false。 删除目录
+    *
+    * @param fromDir
+    * @param toDir
+    * @return
+    */
+  def mvFiles(fileSystem: FileSystem, fromDir: String, toDir: String): Unit = {
+
+    try {
+
+      fileSystem.globStatus(new Path(fromDir + "/*")).foreach(x => {
+        val hidDir = x.getPath.getName
+        if (!fileSystem.exists(new Path(toDir + "/" + hidDir))) {
+          fileSystem.mkdirs(new Path(toDir + "/" + hidDir))
+        }
+        fileSystem.globStatus(new Path(s"${fromDir}/${hidDir}/*")).foreach(f => {
+          val fname = f.getPath.getName
+          val newPath = new Path(toDir + "/" + hidDir + "/" + fname)
+          fileSystem.rename(f.getPath, newPath)
+        })
+      })
+
+      val fromSize = fileSystem.getContentSummary(new Path(fromDir)).getLength
+      if (fromSize == 0) {
+        fileSystem.delete(new Path(fromDir), true)
+      }
+
+    } catch {
+      case e: Exception => {
+        throw new Exception(s"move files from ${fromDir} to ${toDir} failed." + e.getMessage)
+      }
+    }
+  }
+
+  /**
+    *
+    * @return (是否继续执行, 处理信息)
+    */
+
+  /**
+    * 处理数据目录
+    * 如果存在冲突的目录, 判断是否处理冲突目录 1. 不处理： 直接返回不处理的信息
+    *                                    2. 处理： 移动文件, 将多个冲突目录归整为一个doing目录
+    * 如果不存在冲突目录, 1. 只有do目录, 重命名为doing目录
+    *                 2. 只有doing目录, 且重试次数大于等于1, 返回true
+    *                 3. 其他： 返回false
+    *
+    * @param fileSystem
+    * @param inputPath
+    * @param batchID
+    * @param tryTime
+    * @param ifDeal 是否处理冲突的目录  0-不处理  1-处理
+    * @return (是否继续执行, 处理信息)
+    */
+  def dealDir(fileSystem: FileSystem, inputPath: String, batchID: String, tryTime: Int, ifDeal: String): Tuple2[Boolean, String] = {
+
+    val logger = Logger.getLogger("accessETL")
+
+    val inputLocation = inputPath + "/" + batchID
+    val inputDoingLocation = inputPath + "/" + batchID + "_doing"
+    val inputDoneLocation = inputPath + "/" + batchID + "_done"
+
+    val inputPathExists = fileSystem.exists(new Path(inputLocation))
+    val inputPathDoingExists = fileSystem.exists(new Path(inputDoingLocation))
+    val inputPathDoneExists = fileSystem.exists(new Path(inputDoneLocation))
+
+
+    var msg = ""
+    var conflictDir = ""
+    var ifNext: Boolean = true
+
+    logger.info(s"batchID:${batchID}, conflictFlag:<${inputPathExists}><${inputPathDoingExists}><${inputPathDoneExists}>")
+
+    // 是否存在冲突的目录
+    var ifConflict = (inputPathExists, inputPathDoingExists, inputPathDoneExists) match {
+      case (true, true, false) => true
+      case (true, false, true) => true
+      case (false, true, true) => true
+      case (true, true, true) => true
+      case _ => false
+    }
+
+    msg = msg + s" ifConflict:${ifConflict}, [do, doing, done]:[${inputPathExists}, ${inputPathDoingExists}, ${inputPathDoneExists}]"
+
+    // 存在冲突目录, 但是不处理
+    if (ifConflict && ifDeal != "1") {
+      ifNext = false
+      msg = msg + s", ifConflict:${ifConflict}, ifDealConflict:${ifDeal} "
+    }
+    // 存在冲突目录且处理
+    else if (ifConflict && ifDeal == "1") {
+
+      // 将冲突目录归整为do和doing目录
+      (inputPathExists, inputPathDoingExists, inputPathDoneExists) match {
+        case (true, true, true) => {
+          // 1. 先将doing目录下的文件移动到do目录下, 删除doing目录
+          // 2. 将done目录重命名为doing目录
+          mvFiles(fileSystem, inputDoingLocation, inputLocation)
+          renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)
+        }
+        case (false, true, true) => {
+          // 1. 先将doing目录重命名为do目录
+          // 2. 将done目录重命名为doing目录
+          renameHDFSDir(fileSystem, inputDoingLocation, inputLocation)
+          renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)
+        }
+        case (true, false, true) => {
+          // 将done目录重命名为doing目录
+          renameHDFSDir(fileSystem, inputDoneLocation, inputDoingLocation)
+        }
+        case _ => {
+          null
+        }
+      }
+      // 将do目录下的文件移动到doing目录下
+      mvFiles(fileSystem, inputLocation, inputDoingLocation)
+    }
+    // 不存在冲突目录
+    else {
+      if (inputPathExists) {
+        renameHDFSDir(fileSystem, inputLocation, inputDoingLocation)
+      } else if (inputPathDoingExists && tryTime >= 1) {
+        ifNext = true
+      } else {
+        ifNext = false
+        msg = msg + s", ${inputLocation} not exsits."
+      }
+    }
+
+    (ifNext, msg)
+  }
+
   def main(args: Array[String]): Unit = {
     // println(getTime("1521172762"))/hadoop/.m2/repository/com/typesafe/config/1.2.1/config-1.2.1.jar
-
-
     // val asBytes = Base64.getDecoder.decode("aHR0cDovL2J4ZnNmcy5zeGwubWUvdGVtcC9iM2Y2NDM1YmQzNjc4MzRhMzUwNGE5YzYxMDdmMmRjYS5waHA/bT0x")
-
     // System.out.println(new String(asBytes, "utf-8"))
     val hLoc = "hdfs://cdh-nn-001:8020/hadoop/accesslog/201805101005_doing/1005"
-    val hid = hLoc.substring(hLoc.lastIndexOf("/" )+ 1)
+    val hid = hLoc.substring(hLoc.lastIndexOf("/") + 1)
     println(hid)
     println(getPartitionSize(hid))
   }
